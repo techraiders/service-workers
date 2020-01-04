ca2a60ae7a402d782a8d7ec4e624ded4a7b2a547
Navneet Prakash <hello_navneet@hotmail.com>
2020-01-04 23:01:28 +0530

    Manages updates in service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+// version 1
+
 console.log(`We are a service worker.`);
 
 /** Use below syntax to import multiple files of service workers, rather than multile importScript statements, so that below statement guarantees all script will be downloaded in parallel but will execute in sequence */
