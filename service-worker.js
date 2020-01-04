d0e6e0e4eb355f93e6eeb078d83a2e63a54a5490
Navneet Prakash <hello_navneet@hotmail.com>
2020-01-04 13:40:39 +0530

    Catches exception of not able to load external scripts.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,11 @@
 console.log(`We are a service worker.`);
 
 /** Use below syntax to import multiple files of service workers, rather than multile importScript statements, so that below statement guarantees all script will be downloaded in parallel but will execute in sequence */
-importScripts("events.js", "events2.js");
+
+try {
+  importScripts("events.js", "events2.js");
+} catch (error) {
+  console.log(error);
+}
 
 console.log("After loading the events.js script");
